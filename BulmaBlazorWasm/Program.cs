3f07952ab46496e921057f121b4ed24927502e96
mumtazhaqiqy <mumtazhaqiqy@gmail.com>
2022-03-22 12:20:33 +0100

    Add bulma

--- BulmaBlazorWasm/Program.cs
+++ BulmaBlazorWasm/Program.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddBulmaRazor()
+builder.Services.AddBulmaRazor();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
